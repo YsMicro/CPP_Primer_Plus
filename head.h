0b933e2844a2b1d0630cb1fbfdab0cf7d29368a2
Vojago <ywh2742450799@gmail.com>
2024-11-26 23:44:15 +0800

    默认消息

--- head.h
+++ head.h
@@ -4,20 +4,21 @@
 
 #ifndef CPP_PRIMER_PLUS_HEAD_H
 #define CPP_PRIMER_PLUS_HEAD_H
-
-int chapter_12_main();
-
-int chapter_10_main();
-
-int chapter_11_main();
-
-int chapter_13_main();
-
 int chapter_2_main();
 
 int chapter_3_main();
 
 int chapter_4_main();
+
 int chapter_5_main();
+
+int chapter_10_main();
+
+int chapter_11_main();
+
+int chapter_12_main();
+
+int chapter_13_main();
+
 
 #endif //CPP_PRIMER_PLUS_HEAD_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "head.h"
 
-int main() {
+int main()
+{
     std::cout << "Hello, CLion!" << std::endl;
-//    chapter_10_main();
-//    chapter_11_main();
-//    chapter_12_main();
-    // chapter_13_main();
-    // chapter_2_main();
+    chapter_2_main();
     // chapter_3_main();
     // chapter_4_main();
-    chapter_5_main();
+    // chapter_5_main();
+    // chapter_10_main();
+    // chapter_11_main();
+    // chapter_12_main();
+    // chapter_13_main();
 
     return 0;
 }
