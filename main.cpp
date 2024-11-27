52554e1434de0bba3780585e4e0b54df04e5cc8c
Vojago <ywh2742450799@gmail.com>
2024-11-27 17:22:56 +0800

    默认消息

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main()
     // chapter_5_main();
     // chapter_6_main();
     // chapter_7_main();
-    chapter_8_main();
-    // chapter_10_main();
+    // chapter_8_main();
+    chapter_10_main();
     // chapter_11_main();
     // chapter_12_main();
     // chapter_13_main();
--- src/chapter_10/chapter_10_main.cpp
+++ src/chapter_10/chapter_10_main.cpp
@@ -1,9 +1,14 @@
 //
 // Created by Vojago on 2024/9/18.
 //
-
+#include <iostream>
 #include "chapter_10_head.h"
-int chapter_10_main() {
-    useStock2();
+
+int chapter_10_main()
+{
+    std::cout << "Hello Chapter 10!\n" << std::endl;
+    // useStock2();
+    // code_1003_useStock0();
+    code_1006_useStock1();
     return 0;
 }
